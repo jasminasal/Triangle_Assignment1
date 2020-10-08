0c4e0a616bf8c98495e61815d47115940ecd9cfb
jasmina <jsalim@conestogac.ac.ca>
2020-10-08 14:43:47 -0400

    Add project files.

---
+++ Triangle_Assignment1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Triangle_Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
